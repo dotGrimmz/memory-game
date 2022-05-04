ec0ba70c3c83f2c2291f1a169ece7f002e1d587e
dotGrimmz <rakeemxng@gmail.com>
2022-05-03 21:36:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+const csvJSON = (csv) => {
+  const lines = csv.split("\n");
+  const result = [];
+  const headers = lines[0]
+    .split(",")
+    .map((x) => x.replace(/[|&;$%@"<>()+,]/g, ""));
+  for (let i = 1; i < lines.length; i++) {
+    if (!lines[i]) continue;
+    const obj = {};
+    const currentline = lines[i].split(",");
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
+    }
+    result.push(obj);
+  }
+  return result;
+};
+
+const App = () => {
+  const [movies, setMovie] = useState([]);
+
+  // Used to get Data on Load - useEffect
+  useEffect(() => {
+    // async function called to fetch then setState
+    const getData = async () => {
+      // Makes request to database
+      let res = await axios.get(
+        "https://gist.githubusercontent.com/tiangechen/b68782efa49a16edaf07dc2cdaa855ea/raw/0c794a9717f18b094eabab2cd6a6b9a226903577/movies.csv"
+      );
+
+      // Parses Response if needed
+      let parsedData = csvJSON(res?.data);
+
+      // sets the state with the fetched data
+      setMovie(parsedData);
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ol>
+      {movies.map((movie, index) => (
+        <li key={index}>{movie.Film}</li>
+      ))}
+    </ol>
   );
-}
+};
 
 export default App;
